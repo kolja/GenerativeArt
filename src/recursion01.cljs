9f50255ff35373974df449790f976b08e65b536c
Kolja Wilcke <kolja@apfel.de>
2023-06-28 22:31:28 +0200

    recursion

--- src/recursion01.cljs
+++ src/recursion01.cljs
@@ -8,16 +8,25 @@
   ;; (js/noStroke)
   )
 
-(defn cube [n]
+(defn neg [n] (* -1 n))
+
+(defn drawcube [n]
   (let [color n]
+    (js/rotate (/ js/PI 7))
+    (js/translate (neg (/ n 8)) (neg (/ n 8)))
     (js/fill color 100 100)
-    (js/rect n n n n)
-    (when (> n 0) (cube (- n 5)))))
+    (js/rect (neg n) (neg n) n n)
+    ))
+
+(defn cube [n]
+  (drawcube n)
+  (when (> n 0) (cube (- n 20))))
 
 (defn draw []
   (js/background 230)
-  (cube 250)
-  )
+  (js/translate 300 300)
+  (cube 250))
+
 ;; let a = atan2(mouseY - height / 2, mouseX - width / 2);
 
 (defn ^:dev/after-load start []
