d80ec8787cca0eb41295788d03b45d806d7d6480
Kolja Wilcke <kolja@apfel.de>
2023-07-27 22:32:49 +0400

    drawline works

---
+++ src/drawline.cljs
@@ -0,0 +1,31 @@
+
+(ns drawline
+  "draw lines following the mouse cursor"
+  (:require
+    [goog.object :as g]
+    [p5 :as p5]))
+
+(defn setup []
+  (js/createCanvas js/window.innerWidth js/window.innerHeight)
+  (js/colorMode js/HSL 100)
+  (js/frameRate 10)
+  (def points (atom [])))
+
+(defn take-last-20 [v]
+  (let [add-point (conj v [js/mouseX js/mouseY])]
+    (into [] (take-last 20 add-point))))
+
+(defn draw []
+  (js/background 15 100 70)
+  (js/strokeWeight 3)
+  (swap! points take-last-20)
+  (doseq [p1 (take 10 @points)
+          p2 (take-last 10 @points)]
+    (js/line (get p1 0) (get p1 1) (get p2 0) (get p2 1)))
+)
+
+(doto js/window
+  (g/set "setup" setup)
+  (g/set "draw" draw))
+
+
